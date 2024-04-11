7e6a730398ae3e3f67e6be2a785368dbabfea9b6
Luis Zapata <147733560+kindestdev@users.noreply.github.com>
2024-04-11 10:17:20 -0400

    Removed position absolute from left side of page and changed it with flexBox,added flex wrap,changed JS to empty confirm password content if wrong

--- JavaS.js
+++ JavaS.js
@@ -4,10 +4,15 @@
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
     let errorMessage = document.getElementById("errorMessage");
+    let confirmPwd = document.getElementById("confirmPassword")
 
     if (password !== confirmPassword) {
       errorMessage.textContent = "Passwords don't match!";
-      errorMessage.style.color = "red";  // Set error message color to red
+      errorMessage.style.color = "red";
+      confirmPwd.type = 'text'
+      confirmPwd.value = '';
+      confirmPwd.type = 'password'
+      passwordInput.focus();
       return false; // Prevent form submission if passwords don't match
     } else {
       errorMessage.innerHTML = ""; // Clear any previous error message
